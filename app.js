0821bcf3faec5b3d0f0e9ce388ef2d7d1afed0b5
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2022-11-19 17:32:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
-const engine = require('express-handlebars').engine
+const {engine} = require('express-handlebars')
+
 const path = require('path')
 const routeMixer = require('./Routes/__ROUTE_MIXER__')
 
 const app = express();
+
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', engine({
+    defaultLayout: 'main', 
+    layoutsDir: __dirname + '/views/layouts',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+}));
+
+
 
 routeMixer(app);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
 app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
 app.listen(3000);
